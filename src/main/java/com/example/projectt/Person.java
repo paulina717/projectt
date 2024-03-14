0649e99efae301a186662bce3455ad00f67edb2b
paulina717 <129490621+paulina717@users.noreply.github.com>
2024-03-14 12:04:56 +0100

    Add age field

--- src/main/java/com/example/projectt/Person.java
+++ src/main/java/com/example/projectt/Person.java
@@ -4,10 +4,17 @@ public class Person {
 
     private String firstName;
     private String lastName;
+    private int age;
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
 
     public String getFirstName() {
@@ -24,5 +31,13 @@ public class Person {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
